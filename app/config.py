8fe84aaa999103cb120f44af4b275e033af476e0
daveningfr <davenheng1234234@gmail.com>
2024-11-06 19:53:10 +0800

    fein

--- app/config.py
+++ app/config.py
@@ -1,9 +1,5 @@
-# app/config.py
-
-import os
-
 class Config:
-    # Database configuration - SQLite
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://daven:03D%40ven11@116.86.77.68:3306/appdev'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-1
+    MYSQL_HOST = '116.86.77.68:3306'
+    MYSQL_USER = 'daven'
+    MYSQL_PASSWORD = '03D@ven11'
+    MYSQL_DB = 'appdev'
