c2605d10a4058050035bae421ec4463126c7abd2
daveningfr <davenheng1234234@gmail.com>
2024-11-06 20:33:49 +0800

    idk

--- app/config.py
+++ app/config.py
@@ -6,4 +6,3 @@ class Config:
     # Database configuration - SQLite
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://daven:03D%40ven11@116.86.77.68:3306/appdev'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-1
