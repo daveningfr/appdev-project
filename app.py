14fec3a8d6ecf3368d59d5184459b37e75e9da4b
daveningfr <davenheng1234234@gmail.com>
2024-11-06 20:10:52 +0800

    whoops i accidentally fucked up but allg now

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import pymysql
-from app.config import Config  # If configurations are in a separate file
+from app import app, db
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
-# Initialize a connection function
-# def get_db_connection():
-#     return pymysql.connect(
-#         host=app.config['MYSQL_HOST'],
-#         user=app.config['MYSQL_USER'],
-#         password=app.config['MYSQL_PASSWORD'],
-#         database=app.config['MYSQL_DB'],
-#         cursorclass=pymysql.cursors.DictCursor  # For dictionary-like rows
-#     )
+if __name__ == '__main__':
+    # Create tables within the app context
+    with app.app_context():
+        db.create_all()
+    
+    # Run the app
+    app.run(debug=True)
