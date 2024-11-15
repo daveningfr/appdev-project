540d309372bb5520c68801cfde194b89fcb0286d
daveningfr <davenheng1234234@gmail.com>
2024-11-15 15:45:47 +0800

    change from mysql to sqlite cos its just appdev

--- app.py
+++ app.py
@@ -8,4 +8,3 @@ if __name__ == '__main__':
     # Run the app
     app.run(debug=True)
 
-#fein
--- app/config.py
+++ app/config.py
@@ -1,8 +1,21 @@
-# app/config.py
-
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+WTF_CSRF_ENABLED = True
 
 class Config:
-    # Database configuration - SQLite
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://daven:03D%40ven11@116.86.77.68:3306/appdev'
+    # Base configuration class
+    SECRET_KEY = 'supersecretkey'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = False  # Disable CSRF protection
+    
+
+
+app = Flask(__name__)
+
+# Configuration of the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
