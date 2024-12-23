3d0f75bc0fab35eb4b025b99ca30a803ad05a0d5
daveningfr <davenheng1234234@gmail.com>
2024-12-23 21:35:54 +0800

    setting up corbado

--- app/config.py
+++ app/config.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 from dotenv import load_dotenv
 import os
+from corbado_python_sdk import CorbadoSDK , Config as corbado_config
+
 
 load_dotenv()
 
@@ -12,8 +14,12 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = False  # Disable CSRF protection
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+    CORBADO_CLIENT_SECRET = os.getenv('API_SECRET')
+    CORBADO_PROJECT_ID = os.getenv('PROJECT_ID')
 
 app = Flask(__name__)
 app.config.from_object(Config)
+corbado = corbado_config(project_id = Config.CORBADO_PROJECT_ID, api_secret = Config.CORBADO_CLIENT_SECRET)
+sdk = CorbadoSDK(config=corbado)
 
 db = SQLAlchemy(app)
