362fcb1aff381483e419e28bd86c3b1d876f7720
daveningfr <davenheng1234234@gmail.com>
2024-12-23 20:13:06 +0800

    added more security to the app

--- app/config.py
+++ app/config.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = False  # Disable CSRF protection
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
